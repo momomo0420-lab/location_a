6b48aa3bfcc865f86c81920cfe198e6935455661
momomo0420-lab <momomo0420.lab+github@gmail.com>
2023-10-26 09:10:00 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:location_trial/ui/location_screen.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LocationScreen(),
+    );
+  }
+}
---
+++ lib/ui/location_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LocationScreen extends StatelessWidget {
+  const LocationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('LocationApp'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
